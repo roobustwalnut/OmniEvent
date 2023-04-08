f5514c4728341d44f1de29a326c53bcc010350c0
Chutong Zhou <122658116+roobustwalnut@users.noreply.github.com>
2023-04-08 15:21:30 +0800

    Update run.sh

--- scripts/data_processing/duee/run.sh
+++ scripts/data_processing/duee/run.sh
@@ -1,7 +1,4 @@
 python duee.py \
-    --data_dir ../../../data/original/DuEE1.0 \
-    --save_dir ../../../data/processed/DuEE1.0
+    --data_dir /content/OmniEvent/scripts/data_processing/duee/DuEE1.0/DuEE1.0 \
+    --save_dir /content/OmniEvent/scripts/data_processing/duee/DuEE1.0
 
-python duee-fin.py \
-    --data_dir ../../../data/original/DuEE-fin \
-    --save_dir ../../../data/processed/DuEE-fin
