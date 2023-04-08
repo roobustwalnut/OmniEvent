f9e20296ccbc288ea96138fee23ab00389137863
Chutong Zhou <122658116+roobustwalnut@users.noreply.github.com>
2023-04-08 16:15:27 +0800

    Update kagglerun.sh

--- scripts/data_processing/duee/kagglerun.sh
+++ scripts/data_processing/duee/kagglerun.sh
@@ -1,3 +1,3 @@
 python duee.py \
     --data_dir /kaggle/input/duee10/DuEE1.0/DuEE1.0 \
-    --save_dir /kaggle/input/duee10
+    --save_dir /kaggle/working/OmniEvent/scripts/data_processing/duee/DuEE1.0
